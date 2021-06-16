711236a7ee89ca33e74817ba96565d59b8b5bbaf
Saliya <dinushasaliya@gmail.com>
2021-06-16 10:56:31 +0530

    Initital

---
+++ AlarmFinale/AlarmFinale/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * AlarmFinale.cpp
+ *
+ * Created: 6/16/2021 10:49:53 AM
+ * Author : Saliya
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
