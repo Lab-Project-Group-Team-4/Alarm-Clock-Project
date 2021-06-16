c7a39b7a3a6dbde64725008ab03f05ef6df65def
MSI <dumindubandara1999@gmail.com>
2021-06-16 11:27:44 +0530

    Combined File

---
+++ alarmProjectCombined/alarmProjectCombined/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * alarmProjectCombined.cpp
+ *
+ * Created: 6/16/2021 11:14:48 AM
+ * Author : USER
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
